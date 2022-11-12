f7057de0e24db62b934df459830c80df567c6f4e
DESKTOP-0JL46NU\Ale <ascopa@fi.uba.ar>
2022-11-12 14:04:13 -0300

    Updates

--- U-Net CGAN/test_model.py
+++ U-Net CGAN/test_model.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from datetime import datetime
+from os import listdir
 
 import numpy
 from keras.models import load_model
@@ -10,21 +11,7 @@ from numpy import ones, zeros
 import Utils
 
 # Params to modify before testing
-model_to_test = 'gen_model_4446.h5'
-item_label = Utils.CancerLabel.nv
-
-# Create and save files of test execution
-now = datetime.now()
-parent_dir = os.path.join(os.getcwd(), "generated images")
-directory = str(item_label) + " - " + now.strftime("%d%m%Y-%H%M%S")
-test_model_directory = os.path.join(parent_dir, directory)
-os.mkdir(test_model_directory)
-
-generator_model = load_model(model_to_test)
-# Save generator model
-
-shutil.copyfile(model_to_test, test_model_directory + os.sep + model_to_test)
-
+model_to_test_dir = os.path.join("F:", os.sep, "backup", "Facultad", "Tesis", "DL", "U-Net CGAN", "models_to_test")
 
 def save_plot(examples, n):
     # plot images
@@ -40,21 +27,39 @@ def save_plot(examples, n):
         pyplot.close()
 
 
-samples = 100
-channel = 1
-latent_dim = 100
+for model_to_test in listdir(model_to_test_dir):
 
-# generate images
+    model_path = os.path.join(model_to_test_dir, model_to_test)
+    item_label = Utils.CancerLabel.nv
 
-z_input = Utils.get_noise(latent_dim, samples)
+    # Create and save files of test execution
+    now = datetime.now()
+    parent_dir = os.path.join(os.getcwd(), "generated images")
+    directory = str(item_label) + " - " + now.strftime("%d%m%Y-%H%M%S")
+    test_model_directory = os.path.join(parent_dir, directory)
+    os.mkdir(test_model_directory)
 
-real_images, _ = Utils.get_images_and_labels(samples, 64)
+    generator_model = load_model(model_path, compile=False)
+    # Save generator model
 
-labels_input = ones((samples, 1)) * item_label
+    shutil.copyfile(model_path, test_model_directory + os.sep + model_to_test)
 
-fake_images = generator_model.predict([real_images, labels_input, z_input])
 
-# save generated images
-numpy.save("generated_images", fake_images)
-# plot the result
-save_plot(numpy.load("generated_images.npy"), samples)
+    samples = 100
+    channel = 1
+    latent_dim = 100
+
+    # generate images
+
+    z_input = Utils.get_noise(latent_dim, samples)
+
+    real_images, _ = Utils.get_images_and_labels(samples, 64)
+
+    labels_input = ones((samples, 1)) * item_label
+
+    fake_images = generator_model.predict([real_images, labels_input, z_input])
+
+    # save generated images
+    numpy.save("generated_images", fake_images)
+    # plot the result
+    save_plot(numpy.load("generated_images.npy"), samples)
